f15559e01e20a3fc346a44f5551dfb6168909a2e
Cory Gideon <cory.a.gideon@gmail.com>
2024-08-02 01:51:42 +0100

    Merging Vulkano rework into main

---
+++ src/buffer_creation.rs
@@ -0,0 +1,37 @@
+use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
+
+fn create_buffer() {
+    // TODO: This doesn't actually do anything useful, just shows how to create a buffer
+    // Create a cpu accessible buffer
+    let source_content: Vec<i32> = (0..64).collect();
+    let source = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_SRC,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        source_content,
+    )
+    .expect("failed to create source buffer");
+
+    let destination_content: Vec<i32> = (0..64).map(|_| 0).collect();
+    let destination = Buffer::from_iter(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::TRANSFER_DST,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_HOST
+                | MemoryTypeFilter::HOST_RANDOM_ACCESS,
+            ..Default::default()
+        },
+        destination_content,
+    )
+    .expect("failed to create destination buffer");
+}
